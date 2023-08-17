79c24b4fd98851325a21b81d51b0870759b630a6
Samuel Adesola <55460620+adesolasamuel@users.noreply.github.com>
2023-08-17 23:49:59 +0000

    git

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -39,20 +39,14 @@ const projectList = [
   {
     title: "Download a copy of my CV",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "A comprehensive copy of my CV.",
+    url: "https://samuelfiles.blob.core.windows.net/myfiles/Yusuf Hamdallat Taiwo Comprehensive Resume.docx",
   },
   {
-    title: "My Resume Site",
+    title: "Github",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
-  },
-  {
-    title: "GitHub Codespaces and github.dev",
-    description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "A copy of the source code for the portfolio site.",
+    url: "https://github.com/hamdallatyusuf",
   },
 ];
 
